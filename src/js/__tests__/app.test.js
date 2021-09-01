732243a8b641c3c87244e5701bb3755ea67b8717
Dmitry Diakov <d.diakov@list.ru>
2021-09-01 23:17:00 +0500

    imitial commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,9 +1,12 @@
-import ClickButton from "../ClickButton";
+/* eslint-disable linebreak-style *//* eslint-disable no-param-reassign */
+/* eslint-disable class-methods-use-this *//* eslint-disable no-use-before-define */
+
+import ClickButton from '../ClickButton';
 
 test('test adding tooltip', () => {
-  document.body.innerHTML = `<div class='container'><button class='button'>Нажми меня</button></div>`;
-  let button = new ClickButton('.container');
+  document.body.innerHTML = '<div class=\'container\'><button class=\'button\'>Нажми меня</button></div>';
+  const button = new ClickButton('.container');
   button.createTooltip();
-  let div = document.querySelector('.tooltip')
+  const div = document.querySelector('.tooltip');
   expect(div.className).toEqual('tooltip');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
-import ClickButton from "./ClickButton";
+/* eslint-disable linebreak-style *//* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+/* eslint-disable class-methods-use-this *//* eslint-disable no-console */
 
-let container = new ClickButton('.container');
+import ClickButton from './ClickButton';
+
+const container = new ClickButton('.container');
